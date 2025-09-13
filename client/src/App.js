c4d067758113171828502ea358a5247e283e8d12
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-13 10:02:17 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Collaborative Notebook</h1>
+      {/* Add your components here */}
+    </div>
+  );
+}
+
+export default App;
